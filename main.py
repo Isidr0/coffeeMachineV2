fd1f73ab8fa2247207db40c8ea6be45ceb761a0c
Isidr0 <rtwatson86@gmail.com>
2021-11-13 12:42:55 -0500

    home desktop vcs

--- main.py
+++ main.py
@@ -30,29 +30,33 @@ resources = {
     "coffee": 100,
 }
 
-#Test2
 
 def check_resources(c_choice):
-    print(MENU[c_choice])
+    choice_milk = MENU[c_choice]["ingredients"]["milk"]
+    choice_water = MENU[c_choice]["ingredients"]["water"]
+    choice_coffee = MENU[c_choice]["ingredients"]["coffee"]
+    print(choice_water, choice_milk, choice_coffee)
+    print(resources)
+
 
 turn_machine_off = False
 
 while not turn_machine_off:
-    # TODO: 1. Ask user what coffee they'd like
+    # 1. Ask user what coffee they'd like
 
     coffee_choice = input("What would you like? (espresso/latte/cappuccino): ")
 
-    # TODO: 2. Turn off the coffee machine. Use "off" to end execution.
-
+    # 2. Turn off the coffee machine. Use "off" to end execution.
     if coffee_choice == "off":
         turn_machine_off = True
 
-    # TODO: 3. Print report of current resource values
+    # 3. Print report of current resource values
     elif coffee_choice == "report":
         for r in resources:
             print(f"{r}: {resources[r]}")
 
     # TODO: 4. Check if resources are sufficient when user chooses a drink.
-
     elif coffee_choice == "espresso":
         check_resources("espresso")
+    elif coffee_choice == "latte":
+        check_resources("latte")
