80ee012a041fbb88d1e92ba95851880ff9b4965b
Isidr0 <rtwatson86@gmail.com>
2021-11-13 14:45:48 -0500

    home desktop

--- main.py
+++ main.py
@@ -31,13 +31,50 @@ resources = {
 }
 
 
+current_water = resources["water"] = 500
+current_milk = resources["milk"] = 500
+current_coffee = resources["coffee"] = 500
+money = 0
+
+
 def check_resources(c_choice):
-    choice_milk = MENU[c_choice]["ingredients"]["milk"]
-    choice_water = MENU[c_choice]["ingredients"]["water"]
-    choice_coffee = MENU[c_choice]["ingredients"]["coffee"]
-    print(choice_water, choice_milk, choice_coffee)
+    """Takes input coffee choice. Compares ingredients for choice against available resources."""
+    if c_choice == "espresso":
+        choice_water = MENU[c_choice]["ingredients"]["water"]
+        choice_coffee = MENU[c_choice]["ingredients"]["coffee"]
+        print(choice_water, choice_coffee)
+        if choice_water > current_water:
+            print("Sorry there is not enough water.")
+            return True
+        if choice_coffee > current_coffee:
+            print("Sorry there is not enough coffee.")
+            return True
+    else:
+        choice_milk = MENU[c_choice]["ingredients"]["milk"]
+        choice_water = MENU[c_choice]["ingredients"]["water"]
+        choice_coffee = MENU[c_choice]["ingredients"]["coffee"]
+        print(choice_water, choice_milk, choice_coffee)
+        if choice_water > current_water:
+            print("Sorry there is not enough water.")
+            return True
+        if choice_coffee > current_coffee:
+            print("Sorry there is not enough coffee.")
+            return True
+        if choice_milk > current_milk:
+            print("Sorry there is not enough milk")
+            return True
     print(resources)
 
+
+def process_coins(c_choice):
+    print("Please inset coins.")
+    quarters = float(input("How many quarters?: "))
+    dimes = float(input("How many dimes?: "))
+    nickles = float(input("How many nickles?: "))
+    pennies = float(input("How many pennies?: "))
+    amount = quarters * 0.25 + dimes * 0.10 + nickles * 0.05 + pennies * 0.01
+    print(amount)
+    return amount
 
 turn_machine_off = False
 
@@ -52,11 +89,23 @@ while not turn_machine_off:
 
     # 3. Print report of current resource values
     elif coffee_choice == "report":
-        for r in resources:
-            print(f"{r}: {resources[r]}")
+        print(f"Water: {current_water}")
+        print(f"Milk: {current_milk}")
+        print(f"Coffee: {current_coffee}")
+        print(f"Money: {money}")
 
-    # TODO: 4. Check if resources are sufficient when user chooses a drink.
+    # 4. Check if resources are sufficient when user chooses a drink.
     elif coffee_choice == "espresso":
         check_resources("espresso")
+        turn_machine_off = check_resources("espresso")
     elif coffee_choice == "latte":
         check_resources("latte")
+        turn_machine_off = check_resources("latte")
+    elif coffee_choice == "cappuccino":
+        check_resources("cappuccino")
+        turn_machine_off = check_resources("cappuccino")
+
+
+
+    # If the user has enetered enough money the amount gets added to money for the machine.
+    process_coins(coffee_choice)
